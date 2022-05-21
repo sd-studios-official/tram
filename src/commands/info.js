b99593d56ddb23354f8479c1b412d6cc83f5a688
summerisadev <101482788+summerisadev@users.noreply.github.com>
2022-05-21 15:35:55 +0100

    Added info command

---
+++ src/commands/info.js
@@ -0,0 +1,28 @@
+const { MessageEmbed } = require('discord.js')
+const packageJSON = require('../../package.json')
+
+module.exports = {
+  name: 'info',
+  description: 'Info about Tram.',
+  // aliases: ['getHelp'],
+  category: 'general',
+  slash: true,
+  global: true,
+  ownerOnly: false,
+
+  run: async (commandData) => {
+    let totalSeconds = (commandData.client.uptime / 1000);
+
+    const embed = new MessageEmbed()
+      .setTitle('Tram Info')
+      .setDescription('Tram - The bot that does voice channels right.')
+      .setAuthor({ name: 'SummerDev Studios' })
+      .addFields(
+        { name: 'General Info', value: `**Uptime (Hours):** ${Math.floor(totalSeconds / 3600)}\n**Platform:** ${process.platform}\n**DiscordJS Version:** ${packageJSON.dependencies['discord.js']}` },
+        { name: 'Credits', value: '**Main Developers:** summer.#4040 & GateLogicLive#0001\n**Core Libraries:** [Discord.JS](https://discord.js.org) & [discord-slash-command-handler](https://npmjs.com/discord-slash-command-handler)\n**And you!** Thank you for using Tram. It means the world to us.' }
+      )
+      .setColor('RANDOM')
+
+    commandData.interaction.followUp({ embeds: [embed], ephemeral: true })
+  }
+}
