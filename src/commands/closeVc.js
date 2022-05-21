fee78e42702d8f5e08ce3819d2171fb70ed52ff1
summerisadev <101482788+summerisadev@users.noreply.github.com>
2022-05-21 11:08:08 +0100

    Added VC request.

---
+++ src/commands/closeVc.js
@@ -0,0 +1,83 @@
+const quickdb = require('quick.db');
+const db = quickdb;
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+  name: 'vc-close',
+  description: 'Close your active VC channel.',
+  // aliases: ['getHelp'],
+  category: 'vc',
+  slash: true,
+  global: false,
+  ownerOnly: false,
+
+  run: async (commandData) => {
+    const guild = commandData.client.guilds.cache.get(commandData.guild.id)
+
+    if (!db.get(commandData.user.id)) {
+      const embed1 = new MessageEmbed()
+        .setTitle('Error')
+        .setDescription("You do not appear to have an active vc. Please join [the support server](https://discord.gg/YXxET9b94S) and open a ticket if you do have an active vc linked to your user.\n\nYour user id is in the footer for support purposes.")
+        .setColor('RED')
+        .setFooter({ text: `Powered By Tram | UserID: ${commandData.user.id} | Error Code: gCSwx2` })
+
+      return await commandData.interaction.followUp({ embeds: [embed1], ephemeral: true })
+    }
+
+    const channelId = db.get(commandData.user.id);
+    let fetchedChannel = "";
+
+    try {
+      fetchedChannel = guild.channels.cache.get(channelId);
+    } catch {
+      const embed1 = new MessageEmbed()
+        .setTitle('Error')
+        .setDescription("Something went horribly wrong. Please make sure you are in the server that your VC channel is in.\n\n Please join [the support server](https://discord.gg/YXxET9b94S) and open a ticket if you are in the server.")
+        .setColor('RED')
+        .setFooter({ text: `Powered By Tram | Error Code: mZteUh` })
+
+      return await commandData.interaction.followUp({ embeds: [embed1], ephemeral: true })
+    }
+
+    if (!fetchedChannel) {
+      const embed1 = new MessageEmbed()
+        .setTitle('Error')
+        .setDescription("Please make sure you are in the server that your VC channel is in.\n\n Please join [the support server](https://discord.gg/YXxET9b94S) and open a ticket if you are in the server.")
+        .setColor('RED')
+        .setFooter({ text: `Powered By Tram | Error Code: mZteUh` })
+
+      return await commandData.interaction.followUp({ embeds: [embed1], ephemeral: true })
+    }
+
+    if (!guild.me.permissions.has('MANAGE_CHANNELS')) {
+      const embed1 = new MessageEmbed()
+        .setTitle('Error')
+        .setDescription("Tram does not have permission `MANAGE_CHANNELS`. Please give this permission for Tram to function.\n\nPlease refer to [the docs](docs.tram.summerdev.tk)")
+        .setColor('RED')
+        .setFooter({ text: `Powered By Tram | UserID: ${commandData.user.id} | Error Code: aDu7Pq` })
+
+      return await commandData.interaction.followUp({ embeds: [embed2], ephemeral: true })
+    }
+
+    if (!guild.me.permissions.has('MANAGE_GUILD')) {
+      const embed1 = new MessageEmbed()
+        .setTitle('Error')
+        .setDescription("Tram does not have permission `MANAGE_GUILD`. Please give this permission for Tram to function.\n\nPlease refer to [the docs](docs.tram.summerdev.tk)")
+        .setColor('RED')
+        .setFooter({ text: `Powered By Tram | UserID: ${commandData.user.id} | Error Code: ncRZ9V` })
+
+      return await commandData.interaction.followUp({ embeds: [embed2], ephemeral: true })
+    }
+
+    await fetchedChannel.delete()
+    db.delete(commandData.user.id);
+
+    const embed2 = new MessageEmbed()
+      .setTitle('Success!')
+      .setDescription("All invited users have been invited by DM! More info in DM's.")
+      .setColor('GREEN')
+      .setFooter({ text: 'Powered By Tram' })
+
+    return await commandData.interaction.followUp({ embeds: [embed2], ephemeral: true })
+  }
+}
--- src/commands/vc-request.js
+++ src/commands/vc-request.js
@@ -1,6 +1,6 @@
-const quickdb = require('quick.db')
-const db = quickdb
-const { MessageEmbed } = require('discord.js')
+const quickdb = require('quick.db');
+const db = quickdb;
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
   name: 'vc-request',
@@ -41,9 +41,115 @@ module.exports = {
   run: async (commandData) => {
     const guild = commandData.client.guilds.cache.get(commandData.guild.id)
     const member = guild.members.cache.get(commandData.args[1])
+    let everyoneRole = guild.roles.cache.find(r => r.name === '@everyone')
 
     const user = commandData.client.users.cache.find(u => u.id === commandData.args[1])
 
-    await commandData.interaction.followUp({ content: `${member} - ${user}` })
+    // the third and forth users are set to the author to prevent errors i dont know how to fix
+    let user2 = commandData.user
+    let user3 = commandData.user
+    let totalUserCount = 2
+    let userDmd = false
+
+    if (commandData.args[2]) {
+      user2 = guild.members.cache.get(commandData.args[2])
+      totalUserCount = 3
+    }
+
+    if (commandData.args[3]) {
+      user3 = guild.members.cache.get(commandData.args[3])
+      totalUserCount = 4
+    }
+
+    // await commandData.interaction.followUp({ content: `${user} - ${user2} - ${user3}` })
+
+    if (db.get(commandData.user.id)) {
+      const embed1 = new MessageEmbed()
+        .setTitle('Error')
+        .setDescription("For rate-limiting purposes, each user can only have 1 channel linked to a user at any time.\nWe are working on a fix so hold tight!\n\nTo close your current channel, run `/vc-close`!")
+        .setColor('RED')
+        .setFooter({ text: 'Powered By Tram | Error Code: qN7eg2' })
+
+      return await commandData.interaction.followUp({ embeds: [embed1], ephemeral: true })
+    }
+
+    let channelCode = "";
+    let channelId = 0;
+
+    await guild.channels.create(commandData.args[0], {
+      type: "GUILD_VOICE",
+      userLimit: totalUserCount,
+      reason: `${commandData.args[0]} | Powered By Tram VC Request`,
+      permissionOverwrites: [
+        {
+          id: everyoneRole.id,
+          deny: ['VIEW_CHANNEL', 'CONNECT']
+        },
+        {
+          id: commandData.user.id,
+          allow: ['VIEW_CHANNEL', 'CONNECT']
+        },
+        {
+          id: member.id,
+          allow: ['VIEW_CHANNEL', 'CONNECT']
+        },
+        {
+          id: user2.id,
+          allow: ['VIEW_CHANNEL', 'CONNECT']
+        },
+        {
+          id: user3.id,
+          allow: ['VIEW_CHANNEL', 'CONNECT']
+        }
+      ]
+    })
+      .then((c) => {
+        channelId = c.id;
+      })
+
+    await db.set(commandData.user.id, channelId)
+
+    const embed4 = new MessageEmbed()
+      .setTitle('VC Request | Feedback Data')
+      .addFields(
+        { name: 'Name/Reason', value: commandData.args[0] },
+        { name: 'Users (May be your user due to bug)', value: `<@${member.id}> <@${user2.id}> <@${user3.id}>` }
+      )
+      .setColor('GREEN')
+      .setFooter({ text: 'Powered By Tram | Use `/vc-close` to end the vc! | Bug Code: 7rRNFx' })
+
+    commandData.user.send({ embeds: [embed4] })
+
+    const embed3 = new MessageEmbed()
+      .setTitle('VC Request')
+      .setDescription(`You have been asked to join <#${channelId}> by <@${commandData.user.id}>.`)
+      .setColor('GREYPLE')
+      .setFooter({ text: 'Powered By Tram' })
+
+    user.send({ embeds: [embed3] })
+
+    if (!userDmd) {
+      if (commandData.args[2]) {
+        let user2m = commandData.client.users.fetch(user2.id, false).then((user) => {
+            user.send({ embeds: [embed3] })
+        })
+
+      }
+
+      if (commandData.args[3]) {
+        let user3m = commandData.client.users.fetch(user3.id, false).then((user) => {
+          user.send({ embeds: [embed3] })
+        })
+
+      }
+    }
+
+    const embed2 = new MessageEmbed()
+      .setTitle('Success!')
+      .setDescription("All invited users have been invited by DM! More info in DM's.")
+      .setColor('GREEN')
+      .setFooter({ text: 'Powered By Tram' })
+
+    return await commandData.interaction.followUp({ embeds: [embed2], ephemeral: true })
   }
 }
